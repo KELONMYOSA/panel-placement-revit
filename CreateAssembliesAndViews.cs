323894ae6d5d5935074719e9b24e4f3f04adb408
Vladislav Samoilenko <79825223592@yandex.ru>
2022-11-05 13:50:57 +0300

    made a createAssembliesAndViews button

--- CreateAssembliesAndViews.cs
+++ CreateAssembliesAndViews.cs
@@ -89,8 +89,9 @@ namespace PanelPlacement
                 {
                     viewTemplateNames.Add(viewTemplate.Name);
                 }
+                viewTemplateNames = viewTemplateNames.OrderBy(q => q).ToList();
 
-               //Вызываем окно выбора шаблонов вида
+                //Вызываем окно выбора шаблонов вида
                 var uiAssembliesViews = new UserInterfaceViews(viewTemplateNames);
                 bool tdResViews = (bool)uiAssembliesViews.ShowDialog();
                 if (tdResViews == false)
@@ -158,12 +159,57 @@ namespace PanelPlacement
                                 if (transaction.GetStatus() == TransactionStatus.Committed)
                                 {
                                     transaction.Start(type + " - Создание вида сборки");
+
                                     ViewSection viewPlan = AssemblyViewUtils.CreateDetailSection(doc, assemblyInstance.Id, AssemblyDetailViewOrientation.HorizontalDetail, templatePlan, true);
                                     ViewSection viewFront = AssemblyViewUtils.CreateDetailSection(doc, assemblyInstance.Id, AssemblyDetailViewOrientation.ElevationFront, templateFront, true);
                                     ViewSection viewSection = AssemblyViewUtils.CreateDetailSection(doc, assemblyInstance.Id, AssemblyDetailViewOrientation.DetailSectionB, templateSection, true);
+
+                                    viewPlan.Name = "План - " + type;
+                                    viewFront.Name = "Вид спереди - " + type;
+                                    viewSection.Name = "Разрез - " + type;
+
                                     transaction.Commit();
+
+                                    if (transaction.GetStatus() == TransactionStatus.Committed)
+                                    {
+                                        transaction.Start(type + " - Размещение заголовка");
+
+                                        Element textNoteType = new FilteredElementCollector(doc)
+                                            .OfClass(typeof(TextNoteType))
+                                            .WhereElementIsElementType()
+                                            .Where(p => p.Name.StartsWith("Панель"))
+                                            .First();
+
+                                        XYZ planTextXYZ = viewPlan.Origin + new XYZ((viewPlan.get_BoundingBox(doc.GetElement(viewPlan.GetPrimaryViewId()) as View).Max.X - viewPlan.get_BoundingBox(doc.GetElement(viewPlan.GetPrimaryViewId()) as View).Min.X) / 2,
+                                            viewPlan.get_BoundingBox(doc.GetElement(viewPlan.GetPrimaryViewId()) as View).Max.Y - viewPlan.get_BoundingBox(doc.GetElement(viewPlan.GetPrimaryViewId()) as View).Min.Y, 0);
+                                        TextNote planTextNote = TextNote.Create(doc, viewPlan.Id, planTextXYZ, type, textNoteType.Id);
+                                        XYZ frontTextXYZ = new XYZ(viewSection.Origin.X, viewFront.Origin.Y, viewFront.Origin.Z + viewFront.get_BoundingBox(doc.GetElement(viewFront.GetPrimaryViewId()) as View).Max.Z - viewFront.get_BoundingBox(doc.GetElement(viewFront.GetPrimaryViewId()) as View).Min.Z);
+                                        TextNote frontTextNote = TextNote.Create(doc, viewFront.Id, frontTextXYZ, type, textNoteType.Id);
+                                        XYZ sectionTextXYZ = viewFront.Origin + new XYZ(0, (viewFront.get_BoundingBox(doc.GetElement(viewFront.GetPrimaryViewId()) as View).Max.Y - viewFront.get_BoundingBox(doc.GetElement(viewFront.GetPrimaryViewId()) as View).Min.Y) / 2,
+                                            (viewFront.get_BoundingBox(doc.GetElement(viewFront.GetPrimaryViewId()) as View).Max.Z - viewFront.get_BoundingBox(doc.GetElement(viewFront.GetPrimaryViewId()) as View).Min.Z));
+                                        TextNote sectionTextNote = TextNote.Create(doc, viewSection.Id, sectionTextXYZ, type, textNoteType.Id);
+
+                                        transaction.Commit();
+                                        
+                                        if (transaction.GetStatus() == TransactionStatus.Committed)
+                                        {
+                                            transaction.Start(type + " - Текст заголовка");
+
+                                            XYZ planTextOffset = new XYZ((planTextNote.get_BoundingBox(doc.GetElement(planTextNote.OwnerViewId) as View).Min.X - planTextNote.get_BoundingBox(doc.GetElement(planTextNote.OwnerViewId) as View).Max.X) / 2,
+                                            planTextNote.get_BoundingBox(doc.GetElement(planTextNote.OwnerViewId) as View).Max.Y - planTextNote.get_BoundingBox(doc.GetElement(planTextNote.OwnerViewId) as View).Min.Y, 0);
+                                            ElementTransformUtils.MoveElement(doc, planTextNote.Id, planTextOffset);
+                                            XYZ frontTextOffset = new XYZ((frontTextNote.get_BoundingBox(doc.GetElement(frontTextNote.OwnerViewId) as View).Min.X - frontTextNote.get_BoundingBox(doc.GetElement(frontTextNote.OwnerViewId) as View).Max.X),
+                                            0, frontTextNote.get_BoundingBox(doc.GetElement(frontTextNote.OwnerViewId) as View).Max.Z - frontTextNote.get_BoundingBox(doc.GetElement(frontTextNote.OwnerViewId) as View).Min.Z) / 2;
+                                            ElementTransformUtils.MoveElement(doc, frontTextNote.Id, frontTextOffset);
+                                            XYZ sectionTextOffset = new XYZ(0, (sectionTextNote.get_BoundingBox(doc.GetElement(sectionTextNote.OwnerViewId) as View).Min.Y - sectionTextNote.get_BoundingBox(doc.GetElement(sectionTextNote.OwnerViewId) as View).Max.Y) / 2,
+                                            (sectionTextNote.get_BoundingBox(doc.GetElement(sectionTextNote.OwnerViewId) as View).Max.Z - sectionTextNote.get_BoundingBox(doc.GetElement(sectionTextNote.OwnerViewId) as View).Min.Z)) / 2;
+                                            ElementTransformUtils.MoveElement(doc, sectionTextNote.Id, sectionTextOffset);
+
+                                            transaction.Commit();
+                                        }
+                                    }
                                 }
-                            }
+                            }      
                         }
                     }
                 }
