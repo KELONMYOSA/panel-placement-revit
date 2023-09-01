dd71d6409f22b79b43d176f897f89cd998ada699
Vladislav Samoilenko <79825223592@yandex.ru>
2023-09-02 01:14:19 +0300

    added panels mirroring

--- MirrorThePanel.cs
+++ MirrorThePanel.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using System.Windows.Forms;
 
 namespace PanelPlacement
@@ -34,18 +35,62 @@ namespace PanelPlacement
 
                 IList<ElementType> panelTypes = selectedPanels.Select(p => doc.GetElement(p.GetTypeId()) as ElementType).ToList();
                 panelTypes = panelTypes.GroupBy(u => u.Name).Select(g => g.First()).ToList();
-                IList<string> panelTypeNames = panelTypes.Select(p => p.Name).ToList();
 
                 XYZ pointA = (symmetryLine.Location as LocationCurve).Curve.GetEndPoint(0);
                 XYZ pointB = (symmetryLine.Location as LocationCurve).Curve.GetEndPoint(1);
+                XYZ pointC = pointB + new XYZ(0, 0, 1);
+                Plane plane = Plane.CreateByThreePoints(pointA, pointB, pointC);
 
-                MessageBox.Show("- " + string.Join(", ", panelTypeNames) + "\n" + 
-                    "- " + tablePath + "\n" +
-                    "- " + pointA.ToString() + ", " + pointB.ToString() + "\n",
-                    "Готово!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                using (Transaction transaction = new Transaction(doc))
+                {
+                    // Создание зеркальных типов
+                    IList<ElementType> newPanelTypes = new List<ElementType>();
+                    foreach (ElementType panelType in panelTypes)
+                    {
+                        ElementType newPanelType = new FilteredElementCollector(doc)
+                                .OfCategory(BuiltInCategory.OST_StructuralFraming)
+                                .WhereElementIsElementType()
+                                .Cast<ElementType>()
+                                .Where(p => p.Name.Equals("З-" + panelType.Name))
+                                .FirstOrDefault();
+
+                        if (newPanelType == null)
+                        {
+                            transaction.Start("Создание типа З-" + panelType.Name);
+
+                            newPanelType = panelType.Duplicate("З-" + panelType.Name);
+                            ChangeParamsFromCsv(panelType, newPanelType, tablePath);
+
+                            transaction.Commit();
+                        }
+
+                        newPanelTypes.Add(newPanelType);
+                    }
+
+                    // Отзеркаливание панелей
+                    IList<ElementId> selectedPanelIds = selectedPanels.Select(p => p.Id).ToList();
+                    
+                    transaction.Start("Отзеркаливание панелей");
+
+                    IList<ElementId> mirroredPanelIds = ElementTransformUtils.MirrorElements(doc, selectedPanelIds, plane, true);
+                    foreach (ElementId mirroredPanelId in mirroredPanelIds)
+                    {
+                        Element mirroredPanel = doc.GetElement(mirroredPanelId);
+                        string mirroredPanelTypeName = (doc.GetElement(mirroredPanel.GetTypeId()) as ElementType).Name;
+                        ElementType newType = newPanelTypes.Where(t => t.Name.Equals("З-" + mirroredPanelTypeName)).FirstOrDefault();
+                        mirroredPanel.ChangeTypeId(newType.Id);
+                    }
+
+                    transaction.Commit();
+                }
                 
                 return Result.Succeeded;
             }    
+        }
+
+        private static void ChangeParamsFromCsv(ElementType oldType, ElementType newType, string filePath)
+        {
+
         }
     }
 }
